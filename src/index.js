61debbd54f02fa1c1a1fad7c65b3a0a12b29a6f5
Kristian <talleykristian@gmail.com>
2023-09-13 15:15:12 -0700

    quick readme fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import GlassExample from "./GlassExample";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const image = require("./proofs-red.jpg");
+const largeImage = require("./proofs-green.jpg")
+const urlImage = 'https://64.media.tumblr.com/10054a78a9ba90bd21c3ea516fe21f4b/215852e55928b021-e6/s1280x1920/356a448e67f53798897db0eab8bad5c1ce46578f.jpg';
+const bwUrlImage = 'https://64.media.tumblr.com/0488a9ce0b030bd74afdf833f9429942/a3caf4508a1afde7-0f/s1280x1920/c0d44b570ceea3a0ad1b147b779e2f06c085458e.jpg';
+
+const DemoApp = () => {
+  return (
+    <React.Fragment>
+      <div style={{ padding: "2rem", maxWidth: "832px" }} className="app">
+        <GlassExample image={image} largeImage={largeImage}  />
+      </div>
+    </React.Fragment>
+  );
+};
+
+ReactDOM.render(<DemoApp />, document.getElementById("root"));
